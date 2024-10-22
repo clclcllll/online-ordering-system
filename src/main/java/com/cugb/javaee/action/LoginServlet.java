2faf75dcdacd330532a3e2b7a3928a49ff285175
txyyt <1350697275@qq.com>
2024-10-22 15:10:17 +0800

    wst test

--- src/main/java/com/cugb/javaee/action/LoginServlet.java
+++ src/main/java/com/cugb/javaee/action/LoginServlet.java
@@ -3,5 +3,6 @@ package com.cugb.javaee.action;
 public class LoginServlet {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
     }
 }
